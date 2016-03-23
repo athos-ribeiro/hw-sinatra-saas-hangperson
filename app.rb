41467056dbf0b8dade8c6d1db017e9b414689112
Athos Ribeiro <athoscribeiro@gmail.com>
2016-03-23 18:39:05 -0300

    Finish part 5 of assignment
    Signed-off-by: Arthur Del Esposte <arthurmde@gmail.com>
    Signed-off-by: Athos Ribeiro <athoscribeiro@gmail.com>

--- app.rb
+++ app.rb
@@ -38,8 +38,12 @@ class HangpersonApp < Sinatra::Base
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
-    letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+    begin
+      letter = params[:guess].to_s[0]
+      flash[:message] = "You have already used that letter." unless @game.guess(letter)
+    rescue ArgumentError
+      flash[:message] = "Invalid guess."
+    end
     redirect '/show'
   end
   
@@ -49,18 +53,33 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    status = @game.check_win_or_lose
+
+    if status == :play
+      erb :show
+    else
+      redirect '/' + status.to_s
+    end
   end
   
   get '/win' do
-    ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    status = @game.check_win_or_lose
+
+    if status == :win
+      erb :win
+    else
+      redirect '/show'
+    end
   end
   
   get '/lose' do
-    ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    status = @game.check_win_or_lose
+
+    if status == :lose
+      erb :lose
+    else
+      redirect '/show'
+    end
   end
   
 end
