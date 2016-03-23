433b47940361e1c4e158a2d9171f1cded58b9d75
Athos Ribeiro <athoscribeiro@gmail.com>
2016-03-23 17:39:07 -0300

    Finish part 1 on README.md
    Signed-off-by: Arthur Del Esposte <arthurmde@gmail.com>
    Signed-off-by: Athos Ribeiro <athoscribeiro@gmail.com>

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -10,6 +10,8 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   def self.get_random_word
@@ -19,4 +21,43 @@ class HangpersonGame
     Net::HTTP.post_form(uri ,{}).body
   end
 
+  attr_accessor :word, :guesses, :wrong_guesses
+
+  def guess(letter)
+    if letter.nil? || letter.empty? || letter =~ /[^a-zA-Z]/
+      raise ArgumentError
+    end
+    letter = letter.downcase
+    if self.word.include?(letter)
+      return false if self.guesses.include?(letter)
+      self.guesses << letter 
+    else
+      return false if self.wrong_guesses.include?(letter)
+      self.wrong_guesses << letter
+    end
+
+    true
+  end
+
+  def word_with_guesses
+    mask = ''
+    self.word.each_char do |letter|
+      if self.guesses.include?(letter)
+        mask << letter 
+      else
+        mask << '-' 
+      end
+    end
+
+    mask
+  end
+
+  def check_win_or_lose
+    return :lose if self.wrong_guesses.length >= 7
+    self.word.each_char do |letter|
+      return :play unless self.guesses.include?(letter)
+    end
+
+    :win
+  end
 end
